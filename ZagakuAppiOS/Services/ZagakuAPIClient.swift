af822b71612a821656695d82b6abe074ec76234c
Kyle Annen <kyleannen@gmail.com>
2017-12-06 13:48:48 -0600

    WIP prior to adding mocking library

--- ZagakuAppiOS/Services/ZagakuAPIClient.swift
+++ ZagakuAppiOS/Services/ZagakuAPIClient.swift
@@ -18,18 +18,13 @@ class ZagakuServerAPIClient {
         let urlString = self.getRequestURL(baseURL: baseURL, parameters: parameters)
         let url = URL(string: urlString)
         
-//        let urlSession = URLSession(
-//            configuration: URLSessionConfiguration.default,
-//            delegate: self as? URLSessionDelegate,
-//            delegateQueue: nil)
-        
-//        let task = urlSession.dataTask(with: url!) { (data, response, error) in
         let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
             if let data = data {
                 do {
                     let decoder = JSONDecoder()
                     let deserializedResponse = try decoder.decode([ZagakuDate].self, from: data)
-                    completion(deserializedResponse)
+                    let sortedResponse = deserializedResponse.sorted { $0.start_time! < $1.start_time! }
+                    completion(sortedResponse)
                 } catch let error as NSError {
                     print(error)
                 }
